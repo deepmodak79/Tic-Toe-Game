0a6f0608e9c041506c56078498ce90b303d34b61
deepmodak79 <2020itb008.deep@students.iiests.ac.in>
2025-12-30 23:29:57 +0530

    winner function

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ boxes.forEach((box)=>{
 const checkWinner=()=>{
     for(let pattern of winPatters){
         //console.log(pattern);
-        //console.log(pattern[0],pattern[1],pattern[2]);
-        //console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
+        console.log(pattern[0],pattern[1],pattern[2]);
+        console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
         console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
         
         let posValue1 =boxes[pattern[0]].innerText;
