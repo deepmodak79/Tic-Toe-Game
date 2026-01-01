21488ee05676c5db7082ba7ee3d34f0008a78311
deepmodak79 <2020itb008.deep@students.iiests.ac.in>
2026-01-01 22:08:24 +0530

    Tic-Tac-Toe Game featues update

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 let boxes=document.querySelectorAll(".box");
-let btn=document.querySelector("#reset-btn");
+let resetbtn=document.querySelector("#reset-btn");
+let newGameButton = document.querySelector("#new-btn");
+let messageContainer=document.querySelector(".msg-container");
+let message=document.querySelector("#msg");
+
 
 const winPatters=[
     [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
 ];
 
-var flag= true;
+let flag= true;
+let moveCount = 0;
 
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
+        if (box.innerText !== "") return;
         if(flag==true){
         box.innerHTML="0";
         flag=false;
@@ -23,21 +29,47 @@ boxes.forEach((box)=>{
     })
 })
 
-const checkWinner=()=>{
-    for(let pattern of winPatters){
-        //console.log(pattern);
-        console.log(pattern[0],pattern[1],pattern[2]);
-        console.log(boxes[pattern[0]],boxes[pattern[1]],boxes[pattern[2]]);
-        console.log(boxes[pattern[0]].innerText,boxes[pattern[1]].innerText,boxes[pattern[2]].innerText);
-        
-        let posValue1 =boxes[pattern[0]].innerText;
-        let posValue2 =boxes[pattern[1]].innerText;
-        let posValue3 =boxes[pattern[2]].innerText;
 
-        if(posValue1 !="" && posValue2 !=""&& posValue3 !="" )
-            if(posValue1===posValue2 && posValue2===posValue3){
-                console.log("Winner",posValue1);
-            }
+const showWinner=(Winner)=>{
+    message.innerText=`Congratulations, Winner is ${Winner}`;
+    messageContainer.classList.remove("hide");
+    disableBoxes();
+}
 
+const checkWinner = () => {
+    for (let pattern of winPatters) {
+        let posValue1 = boxes[pattern[0]].innerText;
+        let posValue2 = boxes[pattern[1]].innerText;
+        let posValue3 = boxes[pattern[2]].innerText;
+
+        if (posValue1 !== "" && posValue1 === posValue2 && posValue2 === posValue3) {
+            showWinner(posValue1);
+            return;
+        }
+    }
+};
+
+
+const disableBoxes =() =>{
+    for(box of boxes){
+        box.disabled=true;
     }
 }
+
+const enableBoxes =() =>{
+    for(box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+
+const resetGame=()=>{
+    flag= true;
+    enableBoxes();
+    messageContainer.classList.add("hide");
+}
+
+
+newGameButton.addEventListener("click",resetGame);
+resetbtn.addEventListener("click",resetGame);
